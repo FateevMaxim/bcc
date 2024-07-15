e7b30113de21275a78b64aa01c42a5efe3319e52
Maxim Fateyev <maxim.f@rocketfirm.com>
2024-07-15 23:27:24 +0500

    new functional

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -42,7 +42,7 @@
                             收货人: <b>@yield( 'title_text' )({{ Auth::user()->city }})</b> <br />
                             电话: <b>@yield( 'address_two' )</b>
                         </p>
-                        <p class="mb-6 text-sm text-dark" style="display: none;" id="chinaaddress">{{ \Illuminate\Support\Facades\Auth::user()->branchinfo->address }}{{ Auth::user()->code }}.{{ Auth::user()->city }}</p>
+                        <p class="mb-6 text-sm text-dark" style="display: none;" id="chinaaddress">{{ \Illuminate\Support\Facades\Auth::user()->branchinfo->address }}{{ Auth::user()->code }}.{{ \Illuminate\Support\Facades\Auth::user()->name }}.{{ Auth::user()->city }}</p>
                         <button onclick="copyText()" class="focus:outline-none text-white bg-[#b6da3a] hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Копировать</button>
                     </div>
 
